606c6858b0376335598a4bb3edbb61eb431511bf
FrankieMarie <frankiemarie83@gmail.com>
2018-03-13 13:56:44 -0700

    shows route between two points

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+var directionsService;
+var directionsDisplay;
+
 function initMap() {
+    directionsService = new google.maps.DirectionsService();
+    directionsDisplay = new google.maps.DirectionsRenderer();
     var uluru = {lat: 33.4484, lng: -112.0740};
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 4,
@@ -7,5 +12,29 @@ function initMap() {
     var marker = new google.maps.Marker({
       position: uluru,
       map: map
-    });
+    }); 
+    directionsDisplay.setMap(map);   
 }
+
+//grabbing submit button
+let submit = document.getElementById('submit')
+submit.addEventListener('click', ()=> {
+    calcRoute();
+});
+
+// directions
+function calcRoute() {
+    var start = document.getElementById('start').value;
+    var destination = document.getElementById('destination').value;
+    var request = {
+      origin: start,
+      destination: destination,
+      travelMode: 'BICYCLING'
+    };
+    directionsService.route(request, function(result, status) {
+        console.log(result)
+      if (status == 'OK') {
+        directionsDisplay.setDirections(result);
+      }
+    });
+  }
